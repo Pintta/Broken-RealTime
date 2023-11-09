44d9db6b3adfcde51d32375a592f96893e0cd7a3
Pintta <69728770+Pintta@users.noreply.github.com>
2023-11-09 15:30:30 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,8 +2,8 @@ local h = tonumber(os.date("%H"))
 local m = tonumber(os.date("%M"))
 local s = tonumber(os.date("%S"))
 
-local RockstarLogic = 1
-local loopwhole = 1000 / RockstarLogic
+local GTAVI = 1
+local loopwhole = 1000 / GTAVI
 local looptime = loopwhole % 1 >= 0.5 and math.ceil(loopwhole) or math.floor(loopwhole)
 
 CreateThread(function()
@@ -23,7 +23,7 @@ CreateThread(function()
 		if h >= 24 then
 			h = 0
 		end
-		if timer >= 60 * RockstarLogic then
+		if timer >= 60 * GTAVI then
 			timer = 0
 			TriggerClientEvent('op-RealTime:client:serversync', -1, h, m, s, gta_seconds_per_real_second)
 		end
