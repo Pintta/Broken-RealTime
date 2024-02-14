6d9e7c24e090767006e980158199d13f6009a269
Pintta <69728770+Pintta@users.noreply.github.com>
2024-02-14 18:35:18 +0200

    Update server.lua
    Fixed loops goin away, so tight now

--- server.lua
+++ server.lua
@@ -2,14 +2,10 @@ local h = tonumber(os.date("%H"))
 local m = tonumber(os.date("%M"))
 local s = tonumber(os.date("%S"))
 
-local GTAVI = 1
-local loopwhole = 1000 / GTAVI
-local looptime = loopwhole % 1 >= 0.5 and math.ceil(loopwhole) or math.floor(loopwhole)
-
 CreateThread(function()
 	local timer = 0
 	while true do
-		Wait(looptime)
+		Wait(1000)
 		timer = timer + 1
 		s = s + 1
 		if s >= 60 then
@@ -23,13 +19,13 @@ CreateThread(function()
 		if h >= 24 then
 			h = 0
 		end
-		if timer >= 60 * GTAVI then
+		if timer >= 60 then
 			timer = 0
-			TriggerClientEvent('op-RealTime:client:serversync', -1, h, m, s, gta_seconds_per_real_second)
+			TriggerClientEvent('OikeaKello:TsekkaaAika', -1, h, m, s, 1)
 		end
 	end
 end)
 
-RegisterServerEvent('op-RealTime:server:requesttime', function()
-	TriggerClientEvent('op-RealTime:client:serversync', -1, h, m, s, gta_seconds_per_real_second)
+RegisterServerEvent('OikeaKello:KysyAika', function()
+	TriggerClientEvent('OikeaKello:TsekkaaAika', -1, h, m, s, 1)
 end)
