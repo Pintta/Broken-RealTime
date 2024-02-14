6ad2f61c6ac9c807650bd633d85f4c97e20ae48f
Pintta <69728770+Pintta@users.noreply.github.com>
2024-02-14 18:36:29 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
+author 'https://github.com/Pintta'
+description 'https://github.com/Pintta/op-RealTime'
+version '1.0.0'
 client_script 'client.lua'
 server_script 'server.lua'
